7a4433bce7d72feb7d6159eb2c5e7f2d226f0c57
AntekW34 <antekw34@gmail.com>
2025-10-04 20:55:20 +0200

    Update maps-demo.tsx

--- components/maps-demo.tsx
+++ components/maps-demo.tsx
@@ -1,12 +1,36 @@
+
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { Button, StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+
+function add_marker(){
+
+}
 
 export default function App() {
+  const markers = [
+      {
+      description:"Kubots home'nt",
+      coordinate:{latitude: -34.397, longitude: 150.644}
+      }
+  ];
+  //TODO: link markers to database
+  const markersList = markers.map(marker => 
+    <Marker 
+      description={marker.description}
+      coordinate={marker.coordinate}
+    />
+  )
   return (
+    <>
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}>
+        {markersList}
+      </MapView>
     </View>
+      <Button onPress={add_marker} title="➕ Add marker📌"/>
+    </>
   );
 }
 
